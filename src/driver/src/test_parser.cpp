2b34832999176f8d3e6ddfb97cf5885619077ba5
Martin <ergangmartin@gmail.com>
2025-05-17 15:12:25 +0200

    improve parsing, add support for hex inputs

--- src/driver/src/test_parser.cpp
+++ src/driver/src/test_parser.cpp
@@ -1,11 +1,12 @@
-#include <charconv>
+#include <boost/algorithm/string.hpp>
 #include <boost/lexical_cast.hpp>
 #include <boost/property_tree/xml_parser.hpp>
 #include <driver/test_parser.hpp>
 #include <filesystem>
 #include <iostream>
-#include <boost/algorithm/string/predicate.hpp>
 #include <boost/filesystem.hpp>
+#include <string>
+#include <boost/multiprecision/cpp_int.hpp>
 
 using namespace instrumentation;
 
@@ -26,6 +27,8 @@ uint64_t TestBuffer::byte_count() const
 uint64_t TestBuffer::input_count() const {
     return inputs;
 }
+
+
 
 void TestBuffer::write(auto val, type_of_input_bits type)
 {
@@ -57,7 +60,7 @@ tests parse_dir(const std::string &test_dir)
         }
     }
 
-    if (!has_metadata) throw std::runtime_error("test-suite does not contain metadata.xml file");
+    if (!has_metadata) throw std::runtime_error("Test-suite does not contain metadata.xml file");
 
     return tests;
 }
@@ -76,6 +79,17 @@ unsigned __int128 parse_uint128(const std::string &value_str) {
         }
     }
     return val;
+}
+
+std::string hex_to_dec_str(const std::string& hex_str) {
+    boost::multiprecision::cpp_int num;
+    try {
+        num = boost::multiprecision::cpp_int(hex_str);
+    } catch (...) {
+        return "Invalid hex string";
+    }
+
+    return num.str();
 }
 
 void write_untyped(TestBuffer& buffer, const std::string& value_str) {
@@ -114,73 +128,90 @@ void write_untyped(TestBuffer& buffer, const std::string& value_str) {
             return;
         } catch (...) {}
     }
-
     try {
         double val = boost::lexical_cast<double>(value_str);
         buffer.write(val, type_of_input_bits::FLOAT64);
         return;
     } catch (boost::bad_lexical_cast&) {}
 
-    throw std::runtime_error("Failed to parse " + value_str);
+    std::cout << "Failed to parse: " + value_str << ". Value will be saved as 0." << std::endl;
+    buffer.write(0, type_of_input_bits::UINT8);
 }
 
 void write_typed(TestBuffer& buffer, const std::string& type_str,
                            const std::string& value_str)
 {
     try {
-        if (type_str == "bool")
-            buffer.write(boost::lexical_cast<bool>(value_str), type_of_input_bits::UINT8);
-        if (type_str == "char")
-            buffer.write((char)boost::lexical_cast<int>(value_str), type_of_input_bits::SINT8);
-        if (type_str == "uchar" || type_str == "unsigned char")
-            buffer.write((unsigned char)boost::lexical_cast<unsigned int>(value_str), type_of_input_bits::UINT8);
-        if (type_str == "short")
-            buffer.write(boost::lexical_cast<short>(value_str), type_of_input_bits::SINT16);
-        if (type_str == "ushort" || type_str == "unsigned short")
-            buffer.write(boost::lexical_cast<unsigned short>(value_str), type_of_input_bits::UINT16);
-        if (type_str == "int")
-            buffer.write(boost::lexical_cast<int>(value_str), type_of_input_bits::SINT32);
-        if (type_str == "uint" || type_str == "unsigned int")
-            buffer.write(boost::lexical_cast<unsigned int>(value_str), type_of_input_bits::UINT32);
-        if (type_str == "long")
-            buffer.write(boost::lexical_cast<long>(value_str), sizeof(long) == 4 ? type_of_input_bits::SINT32 : type_of_input_bits::SINT64);
-        if (type_str == "ulong" || type_str == "unsigned long")
-            buffer.write(boost::lexical_cast<unsigned long>(value_str), sizeof(long) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64);
-        if (type_str == "longlong")
-            buffer.write(boost::lexical_cast<long long>(value_str), sizeof(long long) == 4 ? type_of_input_bits::SINT32 : type_of_input_bits::SINT64);
-        if (type_str == "ulonglong" || type_str == "unsigned long long")
-            buffer.write(boost::lexical_cast<unsigned long long>(value_str), sizeof(long long) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64);
-        if (type_str == "float")
-            buffer.write(boost::lexical_cast<float>(value_str), type_of_input_bits::FLOAT32);
-        if (type_str == "double")
-            buffer.write(boost::lexical_cast<double>(value_str), type_of_input_bits::FLOAT64);
-        if (type_str == "size_t")
-            buffer.write(boost::lexical_cast<size_t>(value_str), sizeof(size_t) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64);
-        if (type_str == "loff_t")
-            buffer.write(boost::lexical_cast<loff_t>(value_str), sizeof(loff_t) == 4 ? type_of_input_bits::SINT32 : type_of_input_bits::SINT64);
-        if (type_str == "sector_t")
-            buffer.write(boost::lexical_cast<uint64_t>(value_str), type_of_input_bits::UINT64);
-        if (type_str == "pthread_t")
-            buffer.write(boost::lexical_cast<pthread_t>(value_str), sizeof(loff_t) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64);
-        if (type_str == "u32")
-            buffer.write(boost::lexical_cast<uint32_t>(value_str), type_of_input_bits::UINT32);
-        if (type_str == "pchar")
-            buffer.write(boost::lexical_cast<uint64_t>(value_str), type_of_input_bits::UINT64);
-
-        if (type_str == "int128") {
+        if (type_str == "bool" || type_str == "_Bool") {
+            buffer.write(boost::lexical_cast<bool>(value_str), type_of_input_bits::UINT8); return;
+        }
+        if (type_str == "char") {
+            buffer.write(static_cast<char>(boost::lexical_cast<int>(value_str)), type_of_input_bits::SINT8); return;
+        }
+        if (type_str == "uchar" || type_str == "unsigned char") {
+            buffer.write(static_cast<unsigned char>(boost::lexical_cast<unsigned int>(value_str)), type_of_input_bits::UINT8); return;
+        }
+        if (type_str == "short") {
+            buffer.write(boost::lexical_cast<short>(value_str), type_of_input_bits::SINT16); return;
+        }
+        if (type_str == "ushort" || type_str == "unsigned short") {
+            buffer.write(boost::lexical_cast<unsigned short>(value_str), type_of_input_bits::UINT16); return;
+        }
+        if (type_str == "int") {
+            buffer.write(boost::lexical_cast<int>(value_str), type_of_input_bits::SINT32); return;
+        }
+        if (type_str == "uint" || type_str == "unsigned int" || type_str == "u32") {
+            buffer.write(boost::lexical_cast<unsigned int>(value_str), type_of_input_bits::UINT32); return;
+        }
+        if (type_str == "long") {
+            buffer.write(boost::lexical_cast<long>(value_str), sizeof(long) == 4 ? type_of_input_bits::SINT32 : type_of_input_bits::SINT64); return;
+        }
+        if (type_str == "ulong" || type_str == "unsigned long") {
+            buffer.write(boost::lexical_cast<unsigned long>(value_str), sizeof(long) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64); return;
+        }
+        if (type_str == "longlong") {
+            buffer.write(boost::lexical_cast<long long>(value_str), sizeof(long long) == 4 ? type_of_input_bits::SINT32 : type_of_input_bits::SINT64); return;
+        }
+        if (type_str == "ulonglong" || type_str == "unsigned long long") {
+            buffer.write(boost::lexical_cast<unsigned long long>(value_str), sizeof(long long) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64); return;
+        }
+        if (type_str == "float") {
+            buffer.write(boost::lexical_cast<float>(value_str), type_of_input_bits::FLOAT32); return;
+        }
+        if (type_str == "double") {
+            buffer.write(boost::lexical_cast<double>(value_str), type_of_input_bits::FLOAT64); return;
+        }
+        if (type_str == "size_t") {
+            buffer.write(boost::lexical_cast<size_t>(value_str), sizeof(size_t) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64); return;
+        }
+        if (type_str == "loff_t") {
+            buffer.write(boost::lexical_cast<loff_t>(value_str), sizeof(loff_t) == 4 ? type_of_input_bits::SINT32 : type_of_input_bits::SINT64); return;
+        }
+        if (type_str == "sector_t") {
+            buffer.write(boost::lexical_cast<uint64_t>(value_str), type_of_input_bits::UINT64); return;
+        }
+        if (type_str == "pthread_t") {
+            buffer.write(boost::lexical_cast<pthread_t>(value_str), sizeof(loff_t) == 4 ? type_of_input_bits::UINT32 : type_of_input_bits::UINT64); return;
+        }
+        if (type_str == "pchar") {
+            buffer.write(boost::lexical_cast<uint64_t>(value_str), type_of_input_bits::UINT64); return;
+        }
+        if (type_str == "int128" || type_str == "__int128") {
             __int128 num = parse_uint128(value_str);
             num *= value_str[0] == '-' ? -1 : 1; //UB??
             buffer.write(num, type_of_input_bits::SINT128);
+            return;
         }
-        if (type_str == "uint128") {
+        if (type_str == "uint128" || type_str == "unsigned int128" || type_str == "unsigned __int128") {
             unsigned __int128 num = parse_uint128(value_str);
             buffer.write(num, type_of_input_bits::UINT128);
+            return;
         }
 
-    } catch (const boost::bad_lexical_cast& e) {
-        throw std::runtime_error("Failed to parse " + value_str + " as " +
-                                 type_str + ": " + e.what());
-    }
+    } catch (const boost::bad_lexical_cast&) {}
+
+    std::cout << "Failed to parse " + value_str + " as " + type_str << ". Attempting to parse with deduced type." << std::endl;
+    write_untyped(buffer, value_str);
 }
 
 TestBuffer parse_test(const std::filesystem::path& test_path)
@@ -196,16 +227,27 @@ TestBuffer parse_test(const std::filesystem::path& test_path)
     TestBuffer buffer;
     uint64_t input_count = 0;
 
-    const auto& testcase = pt.get_child("testcase");
+    const auto& testcase = pt.get_child_optional("testcase");
 
-    for (const auto& [name, node] : testcase) {
+    if (!testcase) {
+        return buffer;
+    }
+
+    for (const auto& [name, node] : *testcase) {
         if (name != "input") continue;
 
         auto type_str = node.get<std::string>("<xmlattr>.type", "");
 
         auto data = node.data();
-        if (data.length() == 3 && data[0] == '\'' && data[2]== '\'') {
+
+        boost::trim(data);
+
+        if (data.length() == 3 && ( data[0] == '\'' && data[2]== '\'') || ( data[0] == '\"' && data[2]== '\"')) {
             data = std::to_string(data[1]);
+        }
+
+        if (data.starts_with("0x")) {
+            data = hex_to_dec_str(data);
         }
 
         if (type_str.empty()) {
