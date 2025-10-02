c116221b573dc5ed153dcf4f5a3cc6cd3f6e0a6e
Marek Trtik <trtikm@gmail.com>
2025-10-02 19:24:17 +0200

    Use std::unordered_set instead of boost to see content nicely in debugger.

--- src/driver/include/driver/test_parser.hpp
+++ src/driver/include/driver/test_parser.hpp
@@ -3,15 +3,18 @@
 #include <string>
 #include <vector>
 #include <filesystem>
-#include <boost/unordered_set.hpp>
+#include <unordered_set>
+#include <cstddef>
 
 #include "target/instrumentation_types.hpp"
+
+struct TestBufferHasher;
 
 class TestBuffer {
     std::vector<uint8_t> buffer;
     uint64_t offset = 0;
     uint64_t inputs = 0;
-
+    friend struct TestBufferHasher;
 public:
     TestBuffer();
     void write(auto val, instrumentation::type_of_input_bits type);
@@ -22,14 +25,16 @@ public:
     bool operator==(const TestBuffer& other) const {
         return buffer == other.buffer && offset == other.offset && inputs == other.inputs;
     }
+};
 
-    friend size_t hash_value(const TestBuffer& buf) {
+struct TestBufferHasher {
+    std::size_t operator()(TestBuffer const& buf) const {
         return boost::hash_range(buf.buffer.begin(), buf.buffer.end());
     }
 };
 
 namespace TestDirParser {
-    using tests = boost::unordered_set<TestBuffer>;
+    using tests = std::unordered_set<TestBuffer, TestBufferHasher>;
 
     tests parse_dir(const std::string& test_dir);
 
