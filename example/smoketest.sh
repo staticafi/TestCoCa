679cc28f7c559a603ad581a3700f75709ff89929
Martin <ergangmartin@gmail.com>
2025-12-04 18:44:08 +0100

    add smoketest

---
+++ example/smoketest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 ./TestCoCa.py --input_file example/example.c --test_suite example/test-suite --testcomp
---
+++ smoketest.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 ./dist/TestCoCa.py --input_file dist/benchmarks/cover_branches/int16_equal/int16_equal.c --test_suite dist/benchmarks/cover_branches/int16_equal/test-suite --output_dir smoketest_output
